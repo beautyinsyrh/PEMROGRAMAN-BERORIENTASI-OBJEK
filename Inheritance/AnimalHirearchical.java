a6be4477bd93e7c89b5ccf57dba554536882e5f6
Beauty Insyiroh Nusuvul Layli <143947762+beautyinsyrh@users.noreply.github.com>
2025-04-02 13:29:16 +0700

    Create AnimalHirearchical.java

---
+++ Inheritance/AnimalHirearchical.java
@@ -0,0 +1,12 @@
+
+package Inheritance;
+
+public class AnimalHierarchical {
+   String name;
+   public AnimalHierarchical(){
+       System.out.println("I am an animal");
+   }
+   public void speak(){
+       System.out.println("Hello");
+   }
+}
